abc08a874141464df93241bc845c4e9090023969
HalynaPoryvaieva <lastiklastik407@gmail.com>
2024-02-14 16:14:49 +0100

    Hw13,Task-_2

--- src/lesson__14/Hw13.java
+++ src/lesson__14/Hw13.java
@@ -3,39 +3,28 @@ package lesson__14;
 import lesson__9.HomeWork9;
 
 import java.util.Arrays;
-
 import static lesson__13.Hm12_1.*;
 import static lesson__9.HomeWork9.createRandomArray;
 
 public class Hw13 {
-    //Используя System.currentTimeMillis() замерить время создания массива из 100_000 случайных целых чисел,
+    // Используя System.currentTimeMillis() замерить время создания массива из 100_000 случайных целых чисел,
     // время линейного поиска в нём, время сортировки и время бинарного поиска.
     // Увеличить размерность массива до 100_000_000 и снова замерить время. Какие выводы?
-
-
     public static void main(String[] args) {
-
-
         int[] array = createRandomArray(100000, 100000000);
-        for (int lange : array) {
+        for (int a : array) {
             long startTime = 0;
-
-
             System.out.println("Размер массива: " + array.length);
-
             long starTime = System.currentTimeMillis();
             array = createRandomArray(1000000, 100000000);
             long andTime = System.currentTimeMillis();
-
             System.out.println("Время создания массива: " + (andTime - startTime) + "ms ");
-
 
             starTime = System.currentTimeMillis();
             linerSerhc(array, 50);
             andTime = System.currentTimeMillis();
             System.out.println(" linerSerhc Time:" + (andTime - startTime) + "ms");
             System.out.println(linerSerhc(array, 50));
-
 
             selectionSort(array);
             starTime = System.currentTimeMillis();
