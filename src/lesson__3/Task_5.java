92cbac73edf5a6d0d437ffa2c5dfb321a00b1c4d
HalynaPoryvaieva <lastiklastik407@gmail.com>
2024-02-14 10:37:48 +0100

    lesson10,11,12

---
+++ src/lesson__3/Task_5.java
@@ -0,0 +1,15 @@
+package lesson__3;
+
+public class Task_5 {
+    public static void main(String[] args) {
+        //int x = 3 Чему будет равно значение х после выполнения операции x += x++ ?
+        // Проверьте свое предположение на практике.
+        int x=3;
+
+            x+=x++;
+
+
+
+        System.out.println(x);
+    }
+}
